394c6f1a8bc12997c6af576e897678a191e856b2
Ajay Singh Thakurathi <ajayst53@gmail.com>
2021-09-14 21:20:50 +0530

    Added add workout screen

--- app/src/main/java/com/goazzi/workoutmanager/view/activity/AddWorkoutActivity.kt
+++ app/src/main/java/com/goazzi/workoutmanager/view/activity/AddWorkoutActivity.kt
@@ -1,15 +1,23 @@
 package com.goazzi.workoutmanager.view.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.goazzi.workoutmanager.R
+import com.goazzi.workoutmanager.helper.Constant
 import com.goazzi.workoutmanager.helper.Util
+import com.goazzi.workoutmanager.model.Workout
+import com.goazzi.workoutmanager.viewmodel.WorkoutViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,9 +25,9 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
         private const val TAG = "AddWorkoutActivity"
     }
 
+    private lateinit var viewModel: WorkoutViewModel
     private val isCategorySelected: BooleanArray = BooleanArray(7)
     private lateinit var edtWorkoutName: AppCompatEditText
-    private lateinit var btnSave: AppCompatButton
     private lateinit var imgShoulder: AppCompatImageView
     private lateinit var imgChest: AppCompatImageView
     private lateinit var imgArms: AppCompatImageView
@@ -38,6 +46,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initViews() {
+        viewModel = ViewModelProvider(this).get(WorkoutViewModel::class.java)
         edtWorkoutName = findViewById(R.id.edt_workout_name)
         imgShoulder = findViewById(R.id.img_shoulder)
         imgChest = findViewById(R.id.img_chest)
@@ -46,7 +55,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
         imgAbs = findViewById(R.id.img_abs)
         imgLegs = findViewById(R.id.img_legs)
         imgFullBody = findViewById(R.id.img_full_body)
-        btnSave = findViewById(R.id.btn_save)
+        val btnSave = findViewById<AppCompatButton>(R.id.btn_save)
 
         val clShoulder = findViewById<ConstraintLayout>(R.id.cl_shoulder)
         val clChest = findViewById<ConstraintLayout>(R.id.cl_chest)
@@ -56,6 +65,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
         val clLegs = findViewById<ConstraintLayout>(R.id.cl_legs)
         val clFullBody = findViewById<ConstraintLayout>(R.id.cl_full_body)
 
+        btnSave.setOnClickListener(this)
         clShoulder.setOnClickListener(this)
         clChest.setOnClickListener(this)
         clArms.setOnClickListener(this)
@@ -65,8 +75,64 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
         clFullBody.setOnClickListener(this)
     }
 
+    private fun validate(): Boolean {
+        if (edtWorkoutName.text.toString()
+                    .isBlank()) {
+            Util.showSnackBar(findViewById(R.id.activity_add_workout), getString(R.string.empty_name))
+            return false
+        }
+        if (!(isCategorySelected[0] || isCategorySelected[1] || isCategorySelected[2] || isCategorySelected[3] || isCategorySelected[4] || isCategorySelected[5] || isCategorySelected[6])) {
+            Util.showSnackBar(findViewById(R.id.activity_add_workout), getString(R.string.zero_category))
+            return false
+        }
+        if (isCategorySelected[6] && (isCategorySelected[0] || isCategorySelected[1] || isCategorySelected[2] || isCategorySelected[3] || isCategorySelected[4] || isCategorySelected[5])) {
+            Util.showSnackBar(findViewById(R.id.activity_add_workout), getString(R.string.incompatible_category))
+            return false
+        }
+        return true
+    }
+
+    private fun saveWorkout() {
+        val categoryList: MutableList<String> = mutableListOf()
+        if (isCategorySelected[0]) {
+            categoryList.add(Constant.CATEGORY_SHOULDER)
+        }
+        if (isCategorySelected[1]) {
+            categoryList.add(Constant.CATEGORY_CHEST)
+        }
+        if (isCategorySelected[2]) {
+            categoryList.add(Constant.CATEGORY_ARMS)
+        }
+        if (isCategorySelected[3]) {
+            categoryList.add(Constant.CATEGORY_BACK)
+        }
+        if (isCategorySelected[4]) {
+            categoryList.add(Constant.CATEGORY_ABS)
+        }
+        if (isCategorySelected[5]) {
+            categoryList.add(Constant.CATEGORY_LEGS)
+        }
+        if (isCategorySelected[6]) {
+            categoryList.add(Constant.CATEGORY_FULL_BODY)
+        }
+        var category = categoryList.toString()
+        category = category.replace(Regex("[\\[\\] ]"), "")
+//        val list: List<String> = category.split(",")
+
+        viewModel.insert(Workout(Util.getUUID(), edtWorkoutName.text.toString()
+                .uppercase(), category, Util.getTimeStamp()))
+        finish()
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
+            R.id.btn_save -> {
+                if (validate()) {
+                    saveWorkout()
+                } else {
+                    return
+                }
+            }
             R.id.cl_shoulder -> {
                 isCategorySelected[0] = !isCategorySelected[0]
                 if (isCategorySelected[0]) {
@@ -82,6 +148,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgChest.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
             R.id.cl_arms -> {
                 isCategorySelected[2] = !isCategorySelected[2]
@@ -90,6 +157,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgArms.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
             R.id.cl_back -> {
                 isCategorySelected[3] = !isCategorySelected[3]
@@ -98,6 +166,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgBack.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
             R.id.cl_abs -> {
                 isCategorySelected[4] = !isCategorySelected[4]
@@ -106,6 +175,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgAbs.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
             R.id.cl_legs -> {
                 isCategorySelected[5] = !isCategorySelected[5]
@@ -114,6 +184,7 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgLegs.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
             R.id.cl_full_body -> {
                 isCategorySelected[6] = !isCategorySelected[6]
@@ -122,7 +193,16 @@ class AddWorkoutActivity : AppCompatActivity(), View.OnClickListener {
                 } else {
                     imgFullBody.setColorFilter(ContextCompat.getColor(applicationContext, R.color.icon_color), android.graphics.PorterDuff.Mode.SRC_IN);
                 }
+                hideKeyboard()
             }
+        }
+    }
+
+    private fun hideKeyboard() {
+        //hide keyboard
+        this.currentFocus?.let { view ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
 }
